a5c70bec78c9050ef2e96b9752357f1207e67a60
Josh <6891850+malarkeykazoo@users.noreply.github.com>
2021-02-10 12:08:05 -0600

    Update script.js
    fixed spacing

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function init(){
   document.getElementById("awesomeForm").addEventListener("submit", f1);
-    function f1() {
-      var name = document.getElementById("fName").value;
-      alert("Thank you, " + name + ". See, nothing bad happened.");
-    }
-    window.addEventListener('load', init);
+  function f1() {
+    var name = document.getElementById("fName").value;
+    alert("Thank you, " + name + ". See, nothing bad happened.");
   }
+  window.addEventListener('load', init);
+}
