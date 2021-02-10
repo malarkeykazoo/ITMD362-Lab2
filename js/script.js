0100de07d4e8d1a622e8379295c7e5ebb08fac2f
Josh <6891850+malarkeykazoo@users.noreply.github.com>
2021-02-10 11:21:17 -0600

    More style tweaks
    Rounded mobile buttons more, added first name field for more personal touch. Updated JS to pull username.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 function init(){
   document.getElementById("awesomeForm").addEventListener("submit", f1);
     function f1() {
-      alert("Thank you. See, nothing bad happened.");
+      var name = document.getElementById("fName").value;
+      alert("Thank you, " + name + ". See, nothing bad happened.");
     }
     window.addEventListener('load', init);
   }
