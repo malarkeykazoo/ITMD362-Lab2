01496154ebfea8efdd0b9e5f580994e280504004
MalarkeyKazoo <6891850+malarkeykazoo@users.noreply.github.com>
2021-02-11 07:24:34 -0600

    Reworked JS to prevent redirect error on GitHub page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 function init(){
-  document.getElementById("awesomeForm").addEventListener("submit", f1);
   function f1() {
+    event.preventDefault()
     var name = document.getElementById("fName").value;
     alert("Thank you, " + name + ". See, nothing bad happened.");
   }
+  document.getElementById("awesomeForm").addEventListener("submit", f1);
   window.addEventListener('load', init);
 }
